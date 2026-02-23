f5a0d4a47e71129649d092bca3e7ba7280116091
0xfave <0xfave@gmail.com>
2026-02-23 11:07:53 +0000

    test

--- tests/src/test_util.rs
+++ tests/src/test_util.rs
@@ -1,48 +1,38 @@
 use litesvm::LiteSVM;
-use solana_sdk::{
-    signature::{Keypair, Signer},
-    system_program,
-    transaction::Transaction,
-    pubkey::Pubkey,
-    instruction::Instruction,
-    account::Account,
-};
-use spl_associated_token_account::get_associated_token_address;
-use spl_token::state::Mint;
+use litesvm_token::{CreateAssociatedTokenAccount, CreateMint};
+use solana_keypair::Keypair;
+use solana_pubkey::Pubkey;
+use solana_signer::Signer;
+use std::str::FromStr;
 use subscription_model::{self, accounts as subs_accounts, instruction as subs_instruction};
 
 pub async fn setup() -> (LiteSVM, Pubkey, Keypair, Keypair) {
     let mut svm = LiteSVM::new();
-    let program_id = Pubkey::from_str("DTdDF7uKkhVp71NjeDo4U4SqSPVsrVxhLgW3f5bADZzs").unwrap();
+    let _program_id = Pubkey::from_str("DTdDF7uKkhVp71NjeDo4U4SqSPVsrVxhLgW3f5bADZzs").unwrap();
 
-    // Create mint account
-    let mint = Keypair::new();
-    let mint_account = Account::new(
-        0,
-        Mint {
-            mint_authority: COption::None,
-            supply: 0,
-            decimals: 6,
-            is_initialized: true,
-            freeze_authority: COption::None,
-        }
-        .pack()
-        .len(),
-        &spl_token::id(),
-    );
-    svm.set_account(mint.pubkey(), mint_account).unwrap();
+    let payer = Keypair::new();
 
-    // Create merchant and user
+    svm.airdrop(&payer.pubkey(), 10_000_000_000).unwrap();
+
+    let mint = CreateMint::new(&mut svm, &payer)
+        .decimals(6)
+        .send()
+        .unwrap();
+
     let merchant = Keypair::new();
     let user = Keypair::new();
 
-    // Airdrop SOL (just add lamports)
-    svm.set_sysvar(&solana_sdk::sysvar::rent::ID, &solana_sdk::sysvar::rent::Rent::default());
     svm.airdrop(&merchant.pubkey(), 1_000_000_000).unwrap();
     svm.airdrop(&user.pubkey(), 1_000_000_000).unwrap();
 
-    // Create ATAs (you'd need to simulate or call the actual create ATA instruction)
-    // For simplicity, you can manually add token accounts with correct ownership.
+    let _merchant_ata = CreateAssociatedTokenAccount::new(&mut svm, &payer, &mint)
+        .owner(&merchant.pubkey())
+        .send()
+        .unwrap();
+    let _user_ata = CreateAssociatedTokenAccount::new(&mut svm, &payer, &mint)
+        .owner(&user.pubkey())
+        .send()
+        .unwrap();
 
-    (svm, mint.pubkey(), merchant, user)
+    (svm, mint, merchant, user)
 }
